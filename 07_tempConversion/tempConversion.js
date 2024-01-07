357f7c26c502c52e35b90181a9c195c54fd1f933
danielpkot <daniel.pkotlyar@gmail.com>
2024-01-07 17:35:55 -0500

    Exercise 7 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  celsius = (fahrenheit-32)*(5/9)
+  if (celsius == 0){
+    return 0
+  }
+  return celsius = +(celsius.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  fahrenheit = (celsius * 9/5) + 32
+  if (fahrenheit == 0){
+    return 0
+  }
+  console.log(fahrenheit.toFixed(1))
+  return fahrenheit = +(fahrenheit.toFixed(1))
+
 };
 
 // Do not edit below this line
