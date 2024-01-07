ecec5bc36d60eff5b8c17f2130fae89b7aa52071
danielpkot <daniel.pkotlyar@gmail.com>
2024-01-07 16:42:50 -0500

    Exercise 4 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,...itemsToBeRemoved) {
+    for (const item of itemsToBeRemoved){
+        if (array.includes(item)){
+            index = array.indexOf(item)
+            array.splice(index,1)
+        }
+    }
+     return array
 };
 
 // Do not edit below this line
