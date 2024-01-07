93549fd5d27136f058ebf2d849c224c2b28254bc
danielpkot <daniel.pkotlyar@gmail.com>
2024-01-07 17:05:03 -0500

    exercise 5 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+    console.log(typeof num1)
+    if ((
+        typeof num1) !== "number" || (typeof num2) !== "number" ||
+        num1 < 0 || num2 <0){
+        return "ERROR";
+    }
+    if (num1 > num2){
+        let temp = num2;
+        num2 = num1; 
+        num1 = temp;
+        console.log(num1)
+        console.log(num2)
+    }
+    let counter = 0;
+    for ( let i = num1; i <=num2; i++  ){
+        counter += i;
+    }
+    return counter;
 };
 
 // Do not edit below this line
